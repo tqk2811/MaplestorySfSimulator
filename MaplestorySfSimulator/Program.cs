942d22185e5db54753d0a78b385dcde40a1d8baf
tqk2811 <tqk2811@gmail.com>
2025-03-13 18:10:52 +0700

    Add project files.

---
+++ MaplestorySfSimulator/Program.cs
@@ -0,0 +1,102 @@
+﻿Dictionary<int, SfData> dict = new()
+{
+    { 15, new SfData(0.30f, 0.0210f, true)  },
+    { 16, new SfData(0.30f, 0.0210f, true)  },
+    { 17, new SfData(0.15f, 0.0680f, true)  },
+    { 18, new SfData(0.15f, 0.0680f)  },
+    { 19, new SfData(0.15f, 0.0850f)  },
+    { 20, new SfData(0.30f, 0.1050f)  },
+    { 21, new SfData(0.15f, 0.1275f)  },
+    { 22, new SfData(0.15f, 0.1700f)  },
+    { 23, new SfData(0.10f, 0.1800f)  },
+    { 24, new SfData(0.10f, 0.1800f)  },
+    { 25, new SfData(0.10f, 0.1800f)  },
+    { 26, new SfData(0.07f, 0.1860f)  },
+    { 27, new SfData(0.05f, 0.1900f)  },
+    { 28, new SfData(0.03f, 0.1940f)  },
+    { 29, new SfData(0.01f, 0.1980f)  },
+};
+Dictionary<int, int> dictResult = new();
+for (int i = 15; i <= 30; i++)
+{
+    dictResult[i] = 0;
+}
+
+bool isSafeGuard = true;
+bool isSundayEvent = true;
+bool isStarCatch = true;
+int thread = Environment.ProcessorCount;
+int totalTry = 1_000_000;
+int tryCount = 0;
+object lockObj = new();
+object lockObj2 = new();
+
+Task[] tasks = new Task[thread];
+for (int i = 0; i < thread; i++)
+{
+    tasks[i] = Task.Factory.StartNew(SfThread, TaskCreationOptions.LongRunning);
+}
+await Task.WhenAll(tasks);
+
+//print result
+Console.WriteLine($"SG:{isSafeGuard}");
+Console.WriteLine($"Sunday Event (-30% destroy below 21):{isSundayEvent}");
+Console.WriteLine($"StarCatch:{isStarCatch}");
+Console.WriteLine($"Total Try:{totalTry}");
+for (int i = 15; i <= 30; i++)
+{
+    float spares = (float)totalTry / dictResult[i];
+    Console.WriteLine($"{i} star => \t{spares:0.00} spares");
+}
+Console.ReadLine();
+
+
+
+//return max result
+int TrySf()
+{
+    int start = isSafeGuard ? 18 : 15;
+    while (start < 30)
+    {
+        switch (dict[start].Enhance(start, isSundayEvent, isStarCatch))
+        {
+            case EnhanceResult.Failure:
+                break;
+
+            case EnhanceResult.Destroy:
+                return start;
+
+            case EnhanceResult.Success:
+                start++;
+                break;
+        }
+    }
+    return start;
+}
+void SfThread()
+{
+    while (true)
+    {
+        lock (lockObj)
+        {
+            if (tryCount >= totalTry)
+            {
+                return;
+            }
+            else
+            {
+                tryCount++;
+                Console.Title = tryCount.ToString();
+            }
+        }
+        int result = TrySf();
+
+        lock (lockObj2)
+        {
+            for (int i = 15; i <= result; i++)
+            {
+                dictResult[i]++;
+            }
+        }
+    }
+}
---
+++ MaplestorySfSimulator/SfData.cs
@@ -0,0 +1,62 @@
+﻿class SfData
+{
+    public SfData(float success, float destroy, bool isAllowSG = false)
+    {
+        this.SuccessRate = success;
+        this.DestroyRate = destroy;
+        this.SuccessRate_StarCatch = success * 1.05f;
+
+        this.SuccessRateAndDestroyRate = success + destroy;
+        SuccessRateAndDestroyRate_StarCatch = SuccessRate_StarCatch + destroy;
+
+        this.SuccessRateAndDestroyRateSunDayEvent = success + (destroy * 0.7f);
+        this.SuccessRateAndDestroyRateSunDayEvent_StarCatch = SuccessRate_StarCatch + (destroy * 0.7f);
+        this.IsAllowSG = isAllowSG;
+    }
+    public bool IsAllowSG { get; set; }
+    public float SuccessRate { get; set; }
+    public float DestroyRate { get; set; }
+    public float FailureRate => 1 - SuccessRate - DestroyRate;
+
+    readonly float SuccessRate_StarCatch;
+    readonly float SuccessRateAndDestroyRate;
+    readonly float SuccessRateAndDestroyRate_StarCatch;
+    readonly float SuccessRateAndDestroyRateSunDayEvent;
+    readonly float SuccessRateAndDestroyRateSunDayEvent_StarCatch;
+    public EnhanceResult Enhance(int currentLevel, bool isSundayEvent, bool isStarCatch)
+    {
+        float random = (float)Random.Shared.NextDouble();
+        if (isStarCatch)
+        {
+            if (random < SuccessRate_StarCatch)
+                return EnhanceResult.Success;
+
+            if (isSundayEvent && currentLevel < 21)
+            {
+                if (random < SuccessRateAndDestroyRateSunDayEvent_StarCatch)
+                    return EnhanceResult.Destroy;
+            }
+            else
+            {
+                if (random < SuccessRateAndDestroyRate_StarCatch)
+                    return EnhanceResult.Destroy;
+            }
+        }
+        else
+        {
+            if (random < SuccessRate)
+                return EnhanceResult.Success;
+            if (isSundayEvent && currentLevel < 21)
+            {
+                if (random < SuccessRateAndDestroyRateSunDayEvent)
+                    return EnhanceResult.Destroy;
+            }
+            else
+            {
+                if (random < SuccessRateAndDestroyRate)
+                    return EnhanceResult.Destroy;
+            }
+        }
+        return EnhanceResult.Failure;
+    }
+}
