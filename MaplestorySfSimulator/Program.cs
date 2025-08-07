86ab4d978c53b8c6e7a273f26001aa1730336c52
tqk2811 <tqk2811@gmail.com>
2025-08-07 23:15:49 +0700

    test

--- MaplestorySfSimulator/Program.cs
+++ MaplestorySfSimulator/Program.cs
@@ -2,11 +2,18 @@
 using MaplestorySfSimulator.SfMax30;
 
 await SimulatorSf25.TestAsync(
+    totalTry: 100_000_000,
+    isSafeGuard: true,
+    isStarCatch: true
     );
 
 Console.WriteLine("==========================================");
 
 await SimulatorSf30.TestAsync(
+    totalTry: 100_000_000,
+    isSafeGuard: true,
+    isSundayEvent: true,
+    isStarCatch: true
     );
 
 
